6ebdcaf057fe1409d33bd5bce05b0897c8c72732
DataKumpel <86472041+DataKumpel@users.noreply.github.com>
2023-03-28 07:14:49 +0200

    Extracted visiting a city into a function; Added OnTriggerExit for leaving a city

--- Assets/Scripts/BuldingEntrance.cs
+++ Assets/Scripts/BuldingEntrance.cs
@@ -11,24 +11,58 @@ public class BuldingEntrance : MonoBehaviour {
         selection_manager = UnitSelectionManager.Connect();
     }
 
+    public void VisitCity(Unit unit) {
+        // Entering a city:
+        var city = building.GetComponent<City>();
+
+        // Check if there is already a visitor in town:
+        if(city.army_visitor != null) {
+            print($"There is already {city.army_visitor} in town... move it away!");
+            return;
+            // TODO (28.3.2023): Show this message in a dialog!
+            // IDEA: Maybe automatically remove the other unit from being visitor???
+        }
+
+        // Enter the building and open city dialog:
+        city.EnterBuilding(unit.owner);
+
+        // Set entering army as visitor:
+        city.UnitEnterBuilding(unit);
+
+        // Deselect the entering unit to avoid unwanted pathing on UI interaction:
+        selection_manager.current_selection.GetComponent<HighlightEffect>().Deselect();
+    }
+
     public void OnTriggerEnter(Collider other) {
         if(other.gameObject == selection_manager.current_selection && building.is_target) {
-            var unit_owner = other.GetComponent<Unit>().owner;
+            var unit = other.GetComponent<Unit>();
             
             if(building.GetComponent<City>() != null) {
-                // Entering a city:
-                var city = building.GetComponent<City>();
-                city.EnterBuilding(unit_owner);
-
-                // Deselect the entering unit to avoid unwanted pathing on UI interaction:
-                selection_manager.current_selection.GetComponent<HighlightEffect>().Deselect();
+                VisitCity(unit);
             } else if(building.GetComponent<ResourceBuilding>() != null) {
                 // Entering a resource building:
                 var resource_building = building.GetComponent<ResourceBuilding>();
-                resource_building.EnterBuilding(unit_owner);
+                resource_building.EnterBuilding(unit.owner);
             } else {
                 // Anything else:
-                building.EnterBuilding(unit_owner);
+                building.EnterBuilding(unit.owner);
+            }
+        }
+    }
+
+    public void OnTriggerExit(Collider other) {
+        // Only GameObjects with a Unit component can trigger:
+        var unit = other.GetComponent<Unit>();
+        if(unit == null) return;
+
+        if(building.GetComponent<City>() != null) {
+            // Leaving a city:
+            var city = building.GetComponent<City>();
+
+            // Only visiting armies can leave the city:
+            if(city.army_visitor == unit.gameObject) {
+                print($"{unit} left city {city}");
+                city.army_visitor = null;
             }
         }
     }
